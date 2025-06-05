7969b6e74312a1f35b4cfdd2fd8462b7f5b7e1ac
Bilenie Tilahun <130017473+Bilenie@users.noreply.github.com>
2025-06-05 15:28:52 -0500

    Update 01_add_supplier.sql

--- insert-update-delete/01_add_supplier.sql
+++ insert-update-delete/01_add_supplier.sql
@@ -1,5 +1,5 @@
 -- Question 1
-INSERT INTO `northwind`.`suppliers` (`CompanyName`, `ContactName`, `ContactTitle`, `Address`, `City`, `Region`, `PostalCode`, `Country`, `Phone`, `Fax`, `HomePage`) VALUES ('Bilenie llc', 'Bilenie Tilahun', 'CEO', '2101 MARTIN', 'GARLAND', 'N.AMERICA', '752222', 'USA', '4561561566', '2445558522', 'BTILAHUN.COM');
+INSERT INTO `northwind`.`suppliers` (`CompanyName`, `ContactName`, `ContactTitle`, `Address`, `City`, `Region`, `PostalCode`, `Country`, `Phone`, `Fax`, `HomePage`) VALUES (null,'Bilenie llc', 'Bilenie Tilahun', 'CEO', '2101 MARTIN', 'GARLAND', 'N.AMERICA', '752222', 'USA', '4561561566', '2445558522', 'BTILAHUN.COM');
 
 -- I run the insert into query twice so got a duplicate that is why I delete and update
 DELETE FROM suppliers WHERE  SupplierID = 32;
